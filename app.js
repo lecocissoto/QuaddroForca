f33bd63983ce8356a9fc18d3de939128a2f98bd6
Leandro Cissoto <lecocissoto@gmail.com>
2016-01-20 11:19:58 -0200

    Adicionei um banco novo

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var routes = require('./routes/index');
 var app = express();
 
 //Connecting to database
-mongoose.connect("mongodb://localhost/quaddro");
+mongoose.connect("mongodb://localhost/forca");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
